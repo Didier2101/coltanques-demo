81c876662ad680bc5e7f447ced23bcaac63f09fa
Didier2101 <didierchavez2101*@gmail.com>
2025-08-26 20:32:47 -0500

    26/08/2025 v9

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="bg-white shadow">
-            <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+            <div className="max-w-8xl mx-auto px-10 py-4 flex justify-between items-center">
                 <div className="flex items-center space-x-3">
                     <div className="w-60 h-16">
                         <img
